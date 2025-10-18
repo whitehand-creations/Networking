63894ac08a400082542b6f35ed0adc1fb727153c
whitehand-creations <whitehand0404@gmail.com>
2025-10-17 22:56:28 -0700

    Add files via upload
    This is a python program that calculates the undirected/directed connections needed for a specific number of nodes inside a full-mesh network topology.

---
+++ fullmesh_calc.py
@@ -0,0 +1,20 @@
+# Calculating the Number of Connections needed in a Full Mesh Network
+
+print("-"*35)
+print("       FULL MESH CALCULATOR          ")
+print("-"*35)
+
+
+while True:
+    n = int(input("Amount of nodes: ")) # Nodes are every one of the connected device on the network
+    undirected_connections = n * (n - 1) // 2 # Undirected connections formula
+    directed_connections = n * (n - 1) # Directed connections formula
+
+    print(f"{undirected_connections} undirected connections required")
+    print(f"{directed_connections} directed connections required")
+    use_again = input("Use again? [Y/N]").strip().upper()
+    print()
+    if use_again != "Y":
+        break
+
+
